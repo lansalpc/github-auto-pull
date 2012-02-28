7a095c729c303ad8a12e9035b01afb151c7fc0bf
WolfieZero <neil@wolfiezero.com>
2012-02-28 17:31:14 +0000

    updated time

--- github.php
+++ github.php
@@ -2,10 +2,21 @@
 
 $projectName	= 'uploadProject';
 $email			= 'neil@wolfiezero.com';
+$remoteIP		= $_SERVER['REMOTE_ADDR'];
+$msg			= 'Request came form '.$remoteIP.' - http://whois.arin.net/rest/ip/'.$remoteIP;
+$password		= '6c78fe881a86738ef8e660a9705cdd5a'; // == th2sc4d
+$salt			= 'th1s54l7y';
 
-if ($_SEVER['REMOTE_ADDR'] === '207.97.227.253') {
-	mail($email, '['.$projectName.'] `GIT PULL` successful', 'Request came form '.$_SERVER['REMOTE_ADDR']);
-	`git pull`;
+if (isset($_GET['check'])) {
+
+	$check = md5(crypt($_GET['check'], $salt));
+
+	if ($password === $check) {
+		mail($email, '['.$projectName.'] `GIT PULL` successful', $msg);
+		`git pull`;
+	} else {
+		mail($email, '['.$projectName.'] `GIT PULL` failed', $msg);
+	}
 } else {
-	mail($email, '['.$projectName.'] `GIT PULL` failed', 'Request came form '.$_SERVER['REMOTE_ADDR']);
+	mail($email, '['.$projectName.'] `GIT PULL` failed', $msg);
 }
